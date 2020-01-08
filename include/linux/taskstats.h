f05ea55d49ccfc7ecc8e664484253d4d0197ef71
kueken <ohnemailadresse@arcor.de>
2020-01-08 01:10:55 +0100

    taskstat.patch

--- include/linux/taskstats.h
+++ include/linux/taskstats.h
@@ -188,6 +188,7 @@ enum {
 	TASKSTATS_TYPE_STATS,		/* taskstats structure */
 	TASKSTATS_TYPE_AGGR_PID,	/* contains pid + stats */
 	TASKSTATS_TYPE_AGGR_TGID,	/* contains tgid + stats */
+	TASKSTATS_TYPE_NULL,		/* contains nothing */
 	__TASKSTATS_TYPE_MAX,
 };
 
